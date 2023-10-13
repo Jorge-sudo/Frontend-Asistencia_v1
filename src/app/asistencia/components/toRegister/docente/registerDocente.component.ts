eab732fe93f0010ac6718dae3ad736fbf98013f7
jorge <jorgearone567@gmail.com>
2023-10-12 22:54:29 -0400

    feature: add-from-docente-0

--- src/app/asistencia/components/toRegister/docente/registerDocente.component.ts
+++ src/app/asistencia/components/toRegister/docente/registerDocente.component.ts
@@ -3,4 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     templateUrl: './registerDocente.component.html'
 })
-export class RegisterDocenteComponent { }
+export class RegisterDocenteComponent {
+  value4: number = 50;
+  dropdownItems = [
+    { name: 'Option 1', code: 'Option 1' },
+    { name: 'Option 2', code: 'Option 2' },
+    { name: 'Option 3', code: 'Option 3' }
+  ];
+  selectedState: any = null;
+}
--- src/app/asistencia/components/toRegister/docente/registerDocente.module.ts
+++ src/app/asistencia/components/toRegister/docente/registerDocente.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterDocenteRoutingModule } from './registerDocente-routing.module';
 import { RegisterDocenteComponent } from './registerDocente.component';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
         CommonModule,
-        RegisterDocenteRoutingModule
+        RegisterDocenteRoutingModule,
+        InputNumberModule,
+        InputTextModule,
+        DropdownModule,
+        FormsModule
     ],
     declarations: [RegisterDocenteComponent]
 })
